6cccf7e6ae8a45d7e516213bd89c79f98e4ebaa0
Revan <Revanab@outlook.com>
2024-06-22 09:53:39 -0500

    todo edits
    Making a more detailed todo 22

--- revan_module.py
+++ revan_module.py
@@ -4,6 +4,15 @@ import random
 import time
 
 
+class StickMan:
+    def __init__(self, screen, x, y, image_filename):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("Mike.png")
+        # self.speed_x
+        # self.speed_y
+
 def main():
     # turn on pygame
     pygame.init()
@@ -11,7 +20,9 @@ def main():
     # create a screen
     pygame.display.set_caption("Cool Project")
     # TODO: Change the size of the screen as you see fit!
-    screen = pygame.display.set_mode((1040, 680))
+    screen = pygame.display.set_mode((1434,805))
+
+    stick_man = StickMan(screen, 200, 150, "Mike.png", )
 
     # let's set the framerate
     clock = pygame.time.Clock()
@@ -21,7 +32,27 @@ def main():
                 sys.exit()
 
             # TODO: Add you events code
-        #stick_man =
+
+
+
+       # pygame.draw.stick_man(screen, (self.x, self.y)
+
+
+
+
+
+        # pressed_keys = pygame.key.get_pressed()
+        # if pressed_keys[pygame.K_RIGHT]:
+        #     stick_man.x = stick_man.x + 10
+        # if pressed_keys[pygame.K_LEFT]:
+        #     stick_man.x = stick_man.x - 10
+        # if pressed_keys[pygame.K_UP]:
+        #     stick_man.y = stick_man.y - 5
+        # if pressed_keys[pygame.K_DOWN]:
+        #     stick_man.y = stick_man.y + 5
+
+
+
 
         # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
