cf01d20a3a832709552e2456c3835333ca17b36d
Revan <Revanab@outlook.com>
2024-06-25 14:14:37 -0500

    todo edits lots of things Making a more detailed todo 22

--- project.py
+++ project.py
@@ -1,55 +1,40 @@
 import pygame
 import sys
-import scoreboard_module
-import  countdown_module
 import main_module
+import time
+import math
+
 
 def main():
+    # turn on pygame
     pygame.init()
-    pygame.display.set_caption("Space Invaders")
-    screen = pygame.display.set_mode((1000, 750))
+    screen_width = 1434
+    screen_height = 805
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    corndog = pygame.image.load("corndog.jpg")
 
-    pygame.mixer.Sound("sounds/win.wav").play()
-
-    fighter_image = pygame.image.load("images/fighter.png").convert()
-    fighter_image.set_colorkey((255, 255, 255))
-
-    badguy_image = pygame.image.load("images/badguy.png")
-
-    play_button = button_module.TextButton(screen, screen.get_width() / 2, 250, "Click here to play")
-
-    instructions_font = pygame.font.Font("fonts/COMIC.TTF", 24)
-    instruction_caption1 = instructions_font.render("Use the left and right arrows to move", True,
-                                                    pygame.Color("White"))
-    instruction_caption2 = instructions_font.render("and the spacebar to fire.  Good luck!", True, pygame.Color("White"))
+    font1 = pygame.font.SysFont("elephant", 100)
+    caption1 = font1.render("TAGGGY", True, pygame.Color("green"))
+    # create a screen
+    # TODO: Change the size of the screen as you see fit!
 
 
-    clock = pygame.time.Clock()
+    pygame.display.set_caption("TAG")
+
     while True:
-        clock.tick(60)
         for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONUP:
-                if play_button.is_clicked_by(event.pos):
-                    print("You clicked the play button.")
-                    main_module.main_game_loop(screen)
             if event.type == pygame.QUIT:
+                pygame.quit()
                 sys.exit()
+
         screen.fill((0, 0, 0))
-
-        x = None  # Just a trick so that x exists after the loop.
-        for k in range(5):
-            x = screen.get_width() / 2 + k * 5
-            y = screen.get_height() - 300 + k * 50
-            pygame.draw.line(screen, (0, 255, 0), (x, y), (x, y + 8), 4)
-        screen.blit(fighter_image, (x - fighter_image.get_width() / 2 + 5, screen.get_height() - fighter_image.get_height()))
-
-        screen.blit(badguy_image, (screen.get_width() / 2 - 100, 100))
-        play_button.draw()
-
-        screen.blit(instruction_caption1, (30, 12))
-        screen.blit(instruction_caption2, (30, 40))
+        screen.blit(corndog, (600, 100))
+        screen.blit(caption1, (screen.get_width() /2 - caption1.get_width() /2 , screen.get_height() / 2))
+        #print(pygame.font.get_fonts())
 
         pygame.display.update()
 
 
-main()
+
+if __name__ == "__main__":
+    main()
